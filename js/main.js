b946c5be457d3950fdf66a18f16f90c3e1553e3d
Shprehen <olandreyts@rambler.ru>
2019-10-27 22:59:22 +0300

    Add some code

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
  $('.header').height($(window).height());
 });
+$('.carousel').carousel({
+    interval: 6000
+});
+
+$(".navbar a").click(function(){
+  $("body,html").animate({
+   scrollTop:$("#" + $(this).data('value')).offset().top
+  },1000)
+  
+ });
